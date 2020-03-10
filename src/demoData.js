8b39297758bef4e6d22f4fed8f4e43a7de09a77b
Taeuk Kang <mail@taeuk.me>
2020-03-10 18:44:02 +0900

    Add more demo data

--- src/demoData.js
+++ src/demoData.js
@@ -1,17 +1,6 @@
 const storesByGeoDemo = {
     stores: [
         {
-            code: "99999999999",
-            name: "(테스트) 라이브 약국",
-            addr: "제주도 제주시 노형동",
-            type: "01",
-            lat: 33.481863,
-            lng: 126.478257,
-            stock_at: "2020/03/10 08:24:30",
-            remain_stat: "plenty",
-            created_at: "2020/03/09 09:00:00"
-        },
-        {
             code: "99999999998",
             name: "(테스트) 일요일 약국",
             addr: "제주도 제주시 연동",
@@ -20,6 +9,61 @@ const storesByGeoDemo = {
             lng: 126.478257,
             stock_at: "2020/03/10 08:24:30",
             remain_stat: "some",
+            created_at: "2020/03/09 09:00:00"
+        },
+        {
+            code: "99999999999",
+            name: "(테스트) 라이브 약국",
+            addr: "제주도 제주시 노형동 asdfasdfsadklfj ajsdklfdjlfs sjdkfljsdl",
+            type: "01",
+            lat: 33.481863,
+            lng: 126.478257,
+            stock_at: "2020/03/10 08:24:30",
+            remain_stat: "plenty",
+            created_at: "2020/03/09 09:00:00"
+        },       
+        {
+            code: "99999999997",
+            name: "(테스트) 우체국",
+            addr: "제주도 제주시 노연로",
+            type: "02",
+            lat: 33.486136,
+            lng: 126.490817,
+            stock_at: "2020/03/10 08:24:30",
+            remain_stat: "few",
+            created_at: "2020/03/09 09:00:00"
+        },
+        {
+            code: "99999999996",
+            name: "(테스트) 농협",
+            addr: "제주도 제주시 연동",
+            type: "03",
+            lat: 33.487329,
+            lng: 126.498848,
+            stock_at: "2020/03/10 08:24:30",
+            remain_stat: "empty",
+            created_at: "2020/03/09 09:00:00"
+        },
+        {
+            code: "99999999997",
+            name: "(테스트) 우체국",
+            addr: "제주도 제주시 노연로",
+            type: "02",
+            lat: 33.486136,
+            lng: 126.490817,
+            stock_at: "2020/03/10 08:24:30",
+            remain_stat: "few",
+            created_at: "2020/03/09 09:00:00"
+        },
+        {
+            code: "99999999996",
+            name: "(테스트) 농협",
+            addr: "제주도 제주시 연동",
+            type: "03",
+            lat: 33.487329,
+            lng: 126.498848,
+            stock_at: "2020/03/10 08:24:30",
+            remain_stat: "empty",
             created_at: "2020/03/09 09:00:00"
         },
         {
