0ca2b1075e6a6bfaa14488e4e797d8dcae59148e
Taeuk Kang <mail@taeuk.me>
2020-03-10 16:58:07 +0900

    Update demo data after API change

--- src/demoData.js
+++ src/demoData.js
@@ -1,29 +1,49 @@
 const storesByGeoDemo = {
     stores: [
         {
+            code: "99999999999",
             name: "(테스트) 라이브 약국",
             addr: "제주도 제주시 노형동",
-            stock_t: "11:40",
-            stock_cnt: "50",
-            sold_cnt: "10",
-            remain_cnt: "10",
-            sold_out: false,
+            type: "01",
             lat: 33.481863,
             lng: 126.478257,
-            code: "99999999999"
+            stock_at: "2020/03/10 08:24:30",
+            remain_stat: "plenty",
+            created_at: "2020/03/09 09:00:00"
         },
         {
+            code: "99999999998",
             name: "(테스트) 일요일 약국",
             addr: "제주도 제주시 연동",
-            stock_t: "11:40",
-            stock_cnt: "100",
-            sold_cnt: "100",
-            remain_cnt: "0",
-            sold_out: true,
-            lat: 33.487697,
-            lng: 126.497763,
-            code: "99999999999"
-        }
+            type: "01",
+            lat: 33.481863,
+            lng: 126.478257,
+            stock_at: "2020/03/10 08:24:30",
+            remain_stat: "some",
+            created_at: "2020/03/09 09:00:00"
+        },
+        {
+            code: "99999999997",
+            name: "(테스트) 우체국",
+            addr: "제주도 제주시 노연로",
+            type: "02",
+            lat: 33.486136,
+            lng: 126.490817,
+            stock_at: "2020/03/10 08:24:30",
+            remain_stat: "few",
+            created_at: "2020/03/09 09:00:00"
+        },
+        {
+            code: "99999999996",
+            name: "(테스트) 농협",
+            addr: "제주도 제주시 연동",
+            type: "03",
+            lat: 33.487329,
+            lng: 126.498848,
+            stock_at: "2020/03/10 08:24:30",
+            remain_stat: "empty",
+            created_at: "2020/03/09 09:00:00"
+        },
     ]
 };
 
