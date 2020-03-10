3ccfada088b916e8a7b2d0182f2a72aa66c89c93
Taeuk Kang <mail@taeuk.me>
2020-03-10 18:06:30 +0900

    Add min-height for map

--- src/components/MapPanel.js
+++ src/components/MapPanel.js
@@ -16,7 +16,7 @@ function MapPanel() {
             <div
                 id="map"
                 ref={mapRef}
-                style={{ width: "100%", height: "100%" }}
+                style={{ width: "100%", height: "100%", minHeight: "300px" }}
             />
         </>
     );
