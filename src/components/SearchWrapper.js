b8578b75a2beccdfc2fedb9359bd53c2e8235b6c
Taeuk Kang <mail@taeuk.me>
2020-07-12 14:31:01 +0900

    Add shutdown notice modal

--- src/components/SearchWrapper.js
+++ src/components/SearchWrapper.js
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { useMaskData } from "../context/MaskDataContext";
 import Search from "./Search";
 import SearchResult from "./SearchResult";
+import { Modal, Button } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
 
 function SearchWrapper() {
     const { centerCoord } = useMaskData();
+    const { t } = useTranslation();
+    const [showModal, setShowModal] = useState(true);
 
-    return <>{!centerCoord ? <Search /> : <SearchResult />}</>;
+    const handleClose = () => setShowModal(false);
+
+    return (
+        <>
+            {!centerCoord ? <Search /> : <SearchResult />}
+            
+            <Modal show={showModal} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>{t("serviceShutdown")}</Modal.Title>
+                </Modal.Header>
+
+                <Modal.Body>
+                    <p>
+                        {t("notice.publicMaskShutdown")}
+                    </p>
+                </Modal.Body>
+
+                <Modal.Footer>
+                    <Button variant="secondary" href="https://livecorona.co.kr">
+                        {t("covid19Dashboard")}
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        {t("close")}
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
 }
 
 export default SearchWrapper;
